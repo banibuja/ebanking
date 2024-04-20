80a5378e3526a139a8cc1483b17c36d3987b4dd9
ArionR8 <149816094+ArionR8@users.noreply.github.com>
2024-04-21 00:14:40 +0200

    logo images

--- src/HeaderNav&Footer/Footer.js
+++ src/HeaderNav&Footer/Footer.js
@@ -1,5 +1,10 @@
 // Navbar.js
 import React, { useEffect } from 'react';
+import footerImg from '../imgs/black&white-logo.png';
+import emailLogo from '../imgs/email-logo (2).png';
+import outlookLogo from '../imgs/outlook-logo.png';
+import twitterLogo from '../imgs/twitter-logo.png';
+import facebookLogo from '../imgs/facebook-logo.png';
 
 const Footer = () => {
   return (
