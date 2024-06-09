0455e3ca4cb14d0fbee474b86ae2f1201f9b1a69
emorina1 <151447538+emorina1@users.noreply.github.com>
2024-06-09 04:00:23 +0200

    add support controller

---
+++ backend/controllers/Support/SupportController.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+const sendMessage = async (req, res) => {
+    const {  message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAILLOGIN,
+        
+            pass: process.env.EMAILPASSWORD,
+        },
+    });
+    const mailOptions = {
+        from: email, 
+        to: 'ebankingebanking7@gmail.com', 
+        replyTo: email,
+        subject: `New message from ${name}`,
+        text: `From: ${name}\nMessage: ${message}`, 
+    };
+    
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json('Email sent successfully').end();
+
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).json('Error sending email').end();
+    }
+};
+
+module.exports = {sendMessage};
