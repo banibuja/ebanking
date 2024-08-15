c6cbac6127aca344a6ee14f4f284df968a3c7fa5
emorina1 <151447538+emorina1@users.noreply.github.com>
2024-08-15 14:14:42 +0200

    Digital Credit

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -174,6 +174,27 @@ export const Dashboard = () => {
                 </div>
               </div>
             )}
+            {role === 'User' && (
+         <>
+         <div className="col-xl-3 col-md-6 mb-4">
+      <div className="card border-left-warning shadow h-100 py-2">
+        <div className="card-body" onClick={() => handleNavigate('/applyloans')}>
+          <div className="row no-gutters align-items-center">
+            <div className="col mr-2">
+              <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                Apply for Loan
+              </div>
+              <div className="h5 mb-0 font-weight-bold text-gray-800">Apply for digital credit</div>
+            </div>
+            <div className="col-auto">
+              <i className="fa fa-credit-card fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+)}
           </div>
         </div>
 
