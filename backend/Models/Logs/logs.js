66d6c7a9e11d03d94a2de60bf1575f579cbd5551
emorina1 <151447538+emorina1@users.noreply.github.com>
2024-09-21 23:25:06 +0200

    Update logs.js
    Ndryshimi i models logs

--- backend/Models/Logs/logs.js
+++ backend/Models/Logs/logs.js
@@ -1,25 +1,31 @@
-
-module.exports =(sequelize,DataTypes)=>{
-    const Logs = sequelize.define('Logs',{
-
-logId:{
-
-},
-	userId:{
-
-    },	action:{
-
-    },
-    	details:{
-
+module.exports = (sequelize, DataTypes) => {
+    const Logs = sequelize.define('Logs', {
+        logId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
         },
-        	timestamp:{
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        action: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        details: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        timestamp: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        tableName: 'logs',
+        timestamps: false
+    });
 
-            },
-            
-  },{
-    tableName:'logs',
-    timestamps: false
-});
-     return Logs;
-};   
+    return Logs;
+};
