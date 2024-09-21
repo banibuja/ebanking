685fb12cbbeb868c3ef1912e747804e6b4b89b47
emorina1 <151447538+emorina1@users.noreply.github.com>
2024-09-21 23:20:43 +0200

    add accesspermissions/models
    eshte shtuar file i accesspermissions/models

--- backend/Models/AccessPermissions/accesspermissions.js
+++ backend/Models/AccessPermissions/accesspermissions.js
@@ -1,19 +1,22 @@
-
-module.exports =(sequelize,DataTypes)=>{
-    const AccessPermissions = sequelize.define('AccessPermissions',{
-        PermissionID:{
-
+module.exports = (sequelize, DataTypes) => {
+    const AccessPermissions = sequelize.define('AccessPermissions', {
+        PermissionID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
         },
-       UserID:{
-
-       },
-       AccessLevel:{
-
-       },
-
-    },{
-        tableName:'accesspermissions',
+        UserID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        AccessLevel: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        tableName: 'accesspermissions',
         timestamps: false
-  });
-       return AccessPermissions;
-};   
+    });
+
+    return AccessPermissions;
+};
