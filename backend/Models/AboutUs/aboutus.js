afd9e4b419619c89f482ed8354cf2af0ce0d2a0b
ArionR8 <149816094+ArionR8@users.noreply.github.com>
2024-09-24 19:58:31 +0200

    add
    add the modul for about us

--- backend/Models/AboutUs/aboutus.js
+++ backend/Models/AboutUs/aboutus.js
@@ -1,18 +1,23 @@
-
-module.exports =(sequelize,DataTypes)=>{
-    const AboutUs = sequelize.define('AboutUs',{
-         AboutUsId:{
-
-        },
-        Tittle:{
-            
-        },
-        Info:{
-
-        },
-    },{
-       tableName:'aboutus',
+module.exports = (sequelize, DataTypes) => {
+   const AboutUs = sequelize.define('AboutUs', {
+       AboutUsId: {
+           type: DataTypes.INTEGER,
+           autoIncrement: true,
+           primaryKey: true,
+           allowNull: false
+       },
+       Tittle: {
+           type: DataTypes.TEXT,
+           allowNull: true
+       },
+       Info: {
+           type: DataTypes.TEXT,
+           allowNull: true
+       }
+   }, {
+       tableName: 'aboutus',
        timestamps: false
-    });
-       return AboutUs;
-};     
+   });
+
+   return AboutUs;
+};
